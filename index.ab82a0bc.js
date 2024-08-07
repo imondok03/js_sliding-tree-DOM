404a41b45792d9edafd4d6b20b1b825ff6ab4b4c
Ivan Mondok <ivanmondok8@gmail.com>
2024-08-07 20:38:13 +0300

    make build

---
+++ index.ab82a0bc.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul");document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.prepend(n.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)}});
+//# sourceMappingURL=index.ab82a0bc.js.map
